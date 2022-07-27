432aed1ad312edd50868575d03d2bfd05a852839
SolTeeth <tmtadeus@gmail.com>
2022-07-27 17:01:55 +0300

    Centered text in form

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -12,10 +12,10 @@ def home(request):
         form = WeatherForm(request.POST)
         if form.is_valid():
             city = form.cleaned_data['city']
-        response = requests.get(f'https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid=037025aea7c9ddf313bf42e7b3136b41').json()
-        lat, lon = response[0]['lat'], response[0]['lon']
+        response = requests.get(f'https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid=037025aea7c9ddf313bf42e7b3136b41').json()[0]
+        lat, lon = response['lat'], response['lon']
         response = requests.get(
             f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang=ru&units=metric&appid=037025aea7c9ddf313bf42e7b3136b41').json()
-        return render(request, 'index.html', {'response': response, 'form': form, 'city': city})
+        return render(request, 'index.html', {'response': response, 'form': form})
     except IndexError:
         return render(request, 'index.html', {'form': form, 'error': 'City you entered does not exist'})
